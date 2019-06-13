4dc89fdac9c607b06978e8b562882e954dfd6d2a
Jonathan Hawkins <jchawkins725@gmail.com>
2019-06-12 20:50:45 -0400

    Updates

---
+++ precache-manifest.82ffce45b90a26fbab6f5ff5c9166703.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cb5751300fb8ebc66bc5bce65e5b40e",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "67f6853f31c5f67d269c",
+    "url": "/pomodoro-clock/static/css/main.f5f5beee.chunk.css"
+  },
+  {
+    "revision": "5b537948f24afc95a481",
+    "url": "/pomodoro-clock/static/js/2.5dbd8ed4.chunk.js"
+  },
+  {
+    "revision": "67f6853f31c5f67d269c",
+    "url": "/pomodoro-clock/static/js/main.6192ea29.chunk.js"
+  },
+  {
+    "revision": "aa817215ef5e490bad59",
+    "url": "/pomodoro-clock/static/js/runtime~main.647dc629.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pomodoro-clock/static/media/logo.5d5d9eef.svg"
+  }
+]);
